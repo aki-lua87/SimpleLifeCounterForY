be3e4cef06cc181c25955838ad8c198d8333a112
aki_lua87 <aki_lua87@outlook.jp>
2018-07-08 04:16:07 +0900

    壁紙を決め打ちで実装、Undoi機能

--- SimpleLifeCounterForY/SimpleLifeCounterForY/ViewModels/MainPageViewModel.cs
+++ SimpleLifeCounterForY/SimpleLifeCounterForY/ViewModels/MainPageViewModel.cs
@@ -2,14 +2,21 @@
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SimpleLifeCounterForY.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private string _bg;
+        public ImageSource Source { get; set; }
+
+        private Stack<(int,int)> UndoStack = new Stack<(int, int)>();
+
         private int _left;
         public int LeftLifePoint
         {
@@ -45,6 +52,8 @@ namespace SimpleLifeCounterForY.ViewModels
         public DelegateCommand Right1DownCommand { get; private set; }
 
         public DelegateCommand LifeResetCommand { get; private set; }
+        public DelegateCommand BackgroungCommand { get; private set; }
+        public DelegateCommand UndoCommand { get; private set; }
 
         public DelegateCommand LeftHerfCommanf { get; private set; }
         public DelegateCommand RightHerfCommanf { get; private set; }
@@ -53,32 +62,34 @@ namespace SimpleLifeCounterForY.ViewModels
         public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
-            Title = "Main Page";
+            Source = ImageSource.FromResource(@"SimpleLifeCounterForY.Resources.bg17.jpg");
 
-            Left1000UpCommand = new DelegateCommand(() => LeftLifePoint += 1000);
-            Left100UpCommand = new DelegateCommand(() => LeftLifePoint += 100);
-            Left10UpCommand = new DelegateCommand(() => LeftLifePoint += 10);
-            Left1UpCommand = new DelegateCommand(() => LeftLifePoint += 1);
+            Left1000UpCommand = new DelegateCommand(() => ChangeLeftLife(1000));
+            Left100UpCommand = new DelegateCommand(() => ChangeLeftLife(100));
+            Left10UpCommand = new DelegateCommand(() => ChangeLeftLife(10));
+            Left1UpCommand = new DelegateCommand(() => ChangeLeftLife(1));
 
-            Left1000DownCommand = new DelegateCommand(() => LeftLifePoint -= 1000);
-            Left100DownCommand = new DelegateCommand(() => LeftLifePoint -= 100);
-            Left10DownCommand = new DelegateCommand(() => LeftLifePoint -= 10);
-            Left1DownCommand = new DelegateCommand(() => LeftLifePoint -= 1);
+            Left1000DownCommand = new DelegateCommand(() => ChangeLeftLife(-1000));
+            Left100DownCommand = new DelegateCommand(() => ChangeLeftLife(-100));
+            Left10DownCommand = new DelegateCommand(() => ChangeLeftLife(-10));
+            Left1DownCommand = new DelegateCommand(() => ChangeLeftLife(-1));
 
-            Right1000UpCommand = new DelegateCommand(() => RightLifePoint += 1000);
-            Right100UpCommand = new DelegateCommand(() => RightLifePoint += 100);
-            Right10UpCommand = new DelegateCommand(() => RightLifePoint += 10);
-            Right1UpCommand = new DelegateCommand(() => RightLifePoint += 1);
+            Right1000UpCommand = new DelegateCommand(() => ChangeRightLife(1000));
+            Right100UpCommand = new DelegateCommand(() => ChangeRightLife(100));
+            Right10UpCommand = new DelegateCommand(() => ChangeRightLife(10));
+            Right1UpCommand = new DelegateCommand(() => ChangeRightLife(1));
 
-            Right1000DownCommand = new DelegateCommand(() => RightLifePoint -= 1000);
-            Right100DownCommand = new DelegateCommand(() => RightLifePoint -= 100);
-            Right10DownCommand = new DelegateCommand(() => RightLifePoint -= 10);
-            Right1DownCommand = new DelegateCommand(() => RightLifePoint -= 1);
+            Right1000DownCommand = new DelegateCommand(() => ChangeRightLife(-1000));
+            Right100DownCommand = new DelegateCommand(() => ChangeRightLife(-100));
+            Right10DownCommand = new DelegateCommand(() => ChangeRightLife(-10));
+            Right1DownCommand = new DelegateCommand(() => ChangeRightLife(-1));
 
             LifeResetCommand = new DelegateCommand(LifeReset);
+            BackgroungCommand = new DelegateCommand(BackgroungChange);
+            UndoCommand = new DelegateCommand(Undo);
 
-            LeftHerfCommanf = new DelegateCommand(() => LeftLifePoint = Convert.ToInt32(Math.Ceiling((double)LeftLifePoint / 2)));
-            RightHerfCommanf = new DelegateCommand(() => RightLifePoint = Convert.ToInt32(Math.Ceiling((double)RightLifePoint / 2)));
+            LeftHerfCommanf = new DelegateCommand(HerfLeftLife);
+            RightHerfCommanf = new DelegateCommand(HerfRightLife);
 
             LifeReset();
         }
@@ -87,6 +98,47 @@ namespace SimpleLifeCounterForY.ViewModels
         {
             RightLifePoint = 8000;
             LeftLifePoint = 8000;
+        }
+
+        private void ChangeLeftLife(int damage)
+        {
+            UndoStack.Push((LeftLifePoint, RightLifePoint));
+            LeftLifePoint += damage;
+        }
+
+        private void ChangeRightLife(int damage)
+        {
+            UndoStack.Push((LeftLifePoint, RightLifePoint));
+            RightLifePoint += damage;
+        }
+
+        private void HerfLeftLife()
+        {
+            UndoStack.Push((LeftLifePoint, RightLifePoint));
+            LeftLifePoint = Convert.ToInt32(Math.Ceiling((double)LeftLifePoint / 2));
+        }
+
+        private void HerfRightLife()
+        {
+            UndoStack.Push((LeftLifePoint, RightLifePoint));
+            RightLifePoint = Convert.ToInt32(Math.Ceiling((double) RightLifePoint / 2));
+        }
+
+        private void Undo()
+        {
+            if (UndoStack.Count == 0)
+            {
+                return;
+            }
+            var que = UndoStack.Pop();
+            LeftLifePoint = que.Item1;
+            RightLifePoint = que.Item2;
+        }
+
+        private void BackgroungChange()
+        {
+            // 動かない
+            Source = ImageSource.FromResource(@"SimpleLifeCounterForY.Resources.bg1.png");
         }
     }
 }
